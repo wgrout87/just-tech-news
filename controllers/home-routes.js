bf03455676236a46460ced9f3d47aa06c1d439c7
Will G <wgrout87@gmail.com>
2022-06-06 18:27:56 -0600

    added session tracking

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,9 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log('======================');
+    console.log(req.session);
+    console.log('======================');
     Post.findAll({
         attributes: [
             'id',
@@ -35,6 +38,15 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
-})
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const session = require('express-session');
+const sequelize = require('./config/connection');
 const hbs = exphbs.create({});
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SuperSecret,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
