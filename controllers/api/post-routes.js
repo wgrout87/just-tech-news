9e75d5536cfc4fb0df0d41320186557660727f00
Will G <wgrout87@gmail.com>
2022-06-20 17:00:25 -0600

    authguard everything

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,6 +3,7 @@ const { rest } = require("lodash");
 const { Post, User, Vote, Comment } = require("../../models");
 const { route } = require("./user-routes");
 const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
   console.log("======================");
@@ -87,11 +88,11 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -100,7 +101,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/upvote", (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   if (req.session) {
     Post.upvote(
       { ...req.body, user_id: req.session.user_id },
@@ -114,7 +115,7 @@ router.put("/upvote", (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -138,7 +139,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
